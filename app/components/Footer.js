a30f641b2d0b204929b1d2befdd455bed072d3c7
nadianielsen <U2404561@rts-365.dk>
2023-05-15 13:55:58 +0200

    editing some stuff here and there

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -4,7 +4,7 @@ import Navigation from "./Navigation";
 
 const Footer = () => {
     return ( 
-        <footer className="w-[32rem] h-16 bg-neutral-300/50 dark:bg-neutral-600/60 fixed bottom-0 rounded-full backdrop-blur-sm my-4 m-auto mx-[35%] shadow-neutral-800/80">
+        <footer className="md:w-[32rem] h-16 bg-neutral-300/50 dark:bg-neutral-600/60 fixed bottom-0 rounded-full backdrop-blur-sm my-4 w-[20rem] mx-16 md:mx-[20%] lg:mx-[35%] shadow-neutral-800/80">
             <Navigation />
         </footer>
      );
